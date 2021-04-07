7521b1554d08ecc583d0b0916cfbba669fc3e338
Maniak003 <ed@ultrastar.ru>
2021-04-07 22:01:28 +0300

    изменено:      STM32L412K8code/Core/Src/main.c 	изменено:      STM32L412K8code/Release/Core/Src/main.o 	изменено:      STM32L412K8code/Release/STM32L412K8.bin 	изменено:      STM32L412K8code/Release/STM32L412K8.elf 	изменено:      STM32L412K8code/Release/STM32L412K8.list 	изменено:      STM32L412K8code/Release/STM32L412K8.map 	изменено:      STM32L412K8code/STM32L412K8.ioc

--- STM32L412K8code/Core/Src/main.c
+++ STM32L412K8code/Core/Src/main.c
@@ -381,7 +381,7 @@ static void MX_TIM15_Init(void)
   htim15.Instance = TIM15;
   htim15.Init.Prescaler = 0;
   htim15.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim15.Init.Period = 500;
+  htim15.Init.Period = 100;
   htim15.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim15.Init.RepetitionCounter = 0;
   htim15.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
