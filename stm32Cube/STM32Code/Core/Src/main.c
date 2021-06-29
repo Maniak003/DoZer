c56543c25c255b5c4f59cbfc3a401a905d6cbb54
Баранаускас Эдвардас <ed@ultrastar.ru>
2021-06-29 14:16:46 +0300

    modified:   kiCAD/SC-STM32L412K8-light/SC-STM32L412K8.kicad_pcb 	modified:   kiCAD/SC-STM32L412K8-light/SC-STM32L412K8.kicad_pcb-bak 	modified:   stm32Cube/STM32Code/.mxproject 	modified:   stm32Cube/STM32Code/.settings/language.settings.xml 	modified:   stm32Cube/STM32Code/Core/Src/main.c 	modified:   stm32Cube/STM32Code/SC4-LCD128x32.ioc 	modified:   stm32Cube/STM32L412K8code/.mxproject 	modified:   stm32Cube/STM32L412K8code/.settings/language.settings.xml 	modified:   stm32Cube/STM32L412K8code/STM32L412K8.ioc

--- stm32Cube/STM32Code/Core/Src/main.c
+++ stm32Cube/STM32Code/Core/Src/main.c
@@ -258,7 +258,7 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
-  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL7;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL4;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -269,10 +269,10 @@ void SystemClock_Config(void)
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
   {
     Error_Handler();
   }
