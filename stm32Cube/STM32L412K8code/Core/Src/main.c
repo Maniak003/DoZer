4aa9e0d4758a0add1543d7ae49f9e9ed74d77207
Maniak003 <ed@ultrastar.ru>
2021-10-24 12:26:59 +0300

    изменено:      stm32Cube/STM32L412K8code/.settings/language.settings.xml 	изменено:      stm32Cube/STM32L412K8code/Core/Src/main.c 	изменено:      stm32Cube/STM32L412K8code/Release/Core/Src/main.o 	изменено:      stm32Cube/STM32L412K8code/Release/STM32L412K8.bin 	изменено:      stm32Cube/STM32L412K8code/Release/STM32L412K8.elf 	изменено:      stm32Cube/STM32L412K8code/Release/STM32L412K8.list 	изменено:      stm32Cube/STM32L412K8code/STM32L412K8.ioc

--- stm32Cube/STM32L412K8code/Core/Src/main.c
+++ stm32Cube/STM32L412K8code/Core/Src/main.c
@@ -774,7 +774,7 @@ static void MX_TIM2_Init(void)
   htim2.Instance = TIM2;
   htim2.Init.Prescaler = 0;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 40;
+  htim2.Init.Period = 56;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
   if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
