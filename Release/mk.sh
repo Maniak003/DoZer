540a4bcb07f56fe8efca4e59963c9418f7f5677b
Баранаускас Эдвардас <ed@ultrastar.ru>
2022-09-15 10:38:43 +0300

    изменено:      Release/all.zip 	изменено:      Release/mk.sh

--- Release/mk.sh
+++ Release/mk.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-zip all.zip ../stm32Cube/STM32L412K8code/Release/STM32L412K8.hex ../Android/AndroidDoZer/app/release/app-release.apk ../kiCAD/SC-STM32L412K8-light/bom/ibom.html ../kiCAD/SC-STM32L412K8-light/gerber/dozer.zip ../Android/AndroidDoZer/app/release/app-release.apk ../FreeCAD/*.stl
+zip all.zip ../stm32Cube/STM32L412K8code/Release/STM32L412K8.hex ../Android/AndroidDoZer/app/release/app-release.apk ../kiCAD/SC-STM32L412K8-light/bom/ibom.html ../kiCAD/SC-STM32L412K8-light/gerber/SC-STM32L412K8.zip ../Android/AndroidDoZer/app/release/app-release.apk ../FreeCAD/*.stl
