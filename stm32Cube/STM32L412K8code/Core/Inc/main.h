2fdcb8a7016a06fa4ca988b02ad745ed0c5ccc12
Maniak003 <ed@ultrastar.ru>
2023-03-03 20:22:35 +0300

    изменено:      stm32Cube/STM32L412K8code/Core/Inc/main.h 	изменено:      stm32Cube/STM32L412K8code/Release/makefile

--- stm32Cube/STM32L412K8code/Core/Inc/main.h
+++ stm32Cube/STM32L412K8code/Core/Inc/main.h
@@ -37,7 +37,7 @@
 #define reservDataSize 6
 #define batteryMeasureInterval 20000
 #define logSize 200
-#define ADC_REF_VOLTAGE 2.8f
+#define ADC_REF_VOLTAGE 3.0f
 /* USER CODE END Header */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
